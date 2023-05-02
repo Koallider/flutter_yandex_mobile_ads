ab4172f7d305b0e360be5cf0e00adce92c2f63c6
koallider <xehxtkf@gmail.com>
2023-05-02 22:09:27 +0700

    sdk version upgrade

--- test/flutter_ironsource_x_test.dart
+++ test/flutter_ironsource_x_test.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('me.koallider.flutter_yandex_mobile_ads');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 }
